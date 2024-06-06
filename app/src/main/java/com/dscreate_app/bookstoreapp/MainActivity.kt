a92e5fc0f605afc795d02a853818d4bfc8eb7bc6
DmitrySlv <dscreateapp@gmail.com>
2024-06-06 17:21:55 +0400

    Добавление считывания данных с firestore и отображение изменений в режиме real-time.

--- app/src/main/java/com/dscreate_app/bookstoreapp/MainActivity.kt
+++ app/src/main/java/com/dscreate_app/bookstoreapp/MainActivity.kt
@@ -3,24 +3,102 @@ package com.dscreate_app.bookstoreapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.dscreate_app.bookstoreapp.ui.theme.BookStoreAppTheme
+import androidx.compose.ui.unit.dp
+import com.dscreate_app.bookstoreapp.data.Book
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+
+    @Composable
+    fun MainScreen() {
         val fs = Firebase.firestore
-        fs.collection("books")
-            .document()
-            .set(mapOf("name" to "My fav book"))
+
+        val list = remember {
+            mutableStateOf(emptyList<Book>())
+        }
+
+        val listener = fs.collection(COLLECTION_NAME)
+            .addSnapshotListener { snapshot, error ->
+            list.value = snapshot?.toObjects(Book::class.java) ?: emptyList()
+        }
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.8f)
+            ) {
+                items(list.value) { book ->
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentWidth()
+                                .padding(15.dp),
+                            text = book.name
+                        )
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                onClick = {
+                    fs.collection(COLLECTION_NAME)
+                        .document()
+                        .set(
+                            Book(
+                                "My book",
+                                "something",
+                                "100",
+                                "fiction",
+                                "url"
+                            )
+                        )
+                }
+            ) {
+                Text(
+                    text = "Add book"
+                )
+            }
+        }
+    }
+
+    companion object {
+        private const val COLLECTION_NAME = "books"
     }
 }
