fbca0df2cbf2fc72ff4a175d5a41bd330a8f73b6
DmitrySlv <dscreateapp@gmail.com>
2024-12-24 23:06:50 +0400

    Добавление проверки на пустой список на главном и избранном экранах и отображение текста "пусто".

--- app/src/main/java/com/dscreate_app/bookstoreapp/main_screen/MainScreen.kt
+++ app/src/main/java/com/dscreate_app/bookstoreapp/main_screen/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.dscreate_app.bookstoreapp.main_screen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,13 +11,17 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import com.dscreate_app.bookstoreapp.add_book_scrren.models.Book
 import com.dscreate_app.bookstoreapp.add_book_scrren.models.Favourite
 import com.dscreate_app.bookstoreapp.login.data.MainScreenDataObj
@@ -54,9 +59,14 @@ fun MainScreen(
         mutableStateOf(BottomBarItem.Home.title)
     }
 
+    val isFavouriteListEmptyState = remember {
+        mutableStateOf(false)
+    }
+
     LaunchedEffect(Unit) {
         getAllFavouritesIds(dbState, navData.uid) { favs ->
             getAllBooks(dbState, favs) { books ->
+                isFavouriteListEmptyState.value = books.isEmpty()
                 booksListState.value = books
             }
         }
@@ -85,6 +95,7 @@ fun MainScreen(
 
                         getAllFavouritesIds(dbState, navData.uid) { favs ->
                             getAllFavouriteBooks(dbState, favs) { books ->
+                                isFavouriteListEmptyState.value = books.isEmpty()
                                 booksListState.value = books
                             }
                         }
@@ -124,6 +135,7 @@ fun MainScreen(
 
                         getAllFavouritesIds(dbState, navData.uid) { favs ->
                             getAllFavouriteBooks(dbState, favs) { books ->
+                                isFavouriteListEmptyState.value = books.isEmpty()
                                 booksListState.value = books
                             }
                         }
@@ -133,6 +145,7 @@ fun MainScreen(
 
                         getAllFavouritesIds(dbState, navData.uid) { favs ->
                             getAllBooks(dbState, favs) { books ->
+                                isFavouriteListEmptyState.value = books.isEmpty()
                                 booksListState.value = books
                             }
                         }
@@ -140,6 +153,20 @@ fun MainScreen(
                 )
             }
         ) { paddingValue -> //отвечает за отступы контента в зависимости от расположения элементов на экране у списка
+
+            if (isFavouriteListEmptyState.value) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Пусто",
+                        fontSize = 20.sp,
+                        color = Color.LightGray
+                    )
+                }
+            }
+
             LazyVerticalGrid(
                 modifier = Modifier
                     .fillMaxSize()
@@ -224,19 +251,23 @@ private fun getAllFavouriteBooks(
     idsList: List<String>,
     onBooks: (List<Book>) -> Unit,
 ) {
-    db.collection("books")
-        .whereIn(FieldPath.documentId(), idsList)
-        .get()
-        .addOnSuccessListener { task ->
-            val booksList = task.toObjects(Book::class.java).map {
-                if (idsList.contains(it.key)) {
-                    it.copy(isFavourite = true)
-                } else {
-                    it
+    if (idsList.isNotEmpty()) {
+        db.collection("books")
+            .whereIn(FieldPath.documentId(), idsList)
+            .get()
+            .addOnSuccessListener { task ->
+                val booksList = task.toObjects(Book::class.java).map {
+                    if (idsList.contains(it.key)) {
+                        it.copy(isFavourite = true)
+                    } else {
+                        it
+                    }
                 }
+                onBooks(booksList)
             }
-            onBooks(booksList)
-        }
+    } else {
+        onBooks(emptyList())
+    }
 }
 
 private fun getAllFavouritesIds(
